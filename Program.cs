2f30ea67f156a2de8f9cae024773240aca93978c
cesarosoriodelarosa <cesarosoriodelarosa@gmail.com>
2022-12-28 08:32:50 -0500

    se refactoriza el codigo segun lo solicitado

--- Program.cs
+++ Program.cs
@@ -17,12 +17,22 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/dbconexion", async ([FromServices] ProductoContext dbContext) =>
 {
     dbContext.Database.EnsureCreated();
-    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
 });
 
 app.MapGet("/api/productos", async ([FromServices] ProductoContext DbContext) =>
 {
     return Results.Ok(DbContext.Productos);
+});
+
+
+app.MapGet("/api/productos/{id}", async ([FromServices] ProductoContext DbContext, [FromRoute] int id) =>
+{
+
+
+
+    return Results.Ok(DbContext.Productos.Find(id));
+
+
 });
 
 app.MapPost("/api/productos", async ([FromServices] ProductoContext DbContext, [FromBody] Producto producto) =>
@@ -35,39 +45,5 @@ app.MapPost("/api/productos", async ([FromServices] ProductoContext DbContext, [
     return Results.Ok();
 });
 
-app.MapPut("/api/productos/{id}", async ([FromServices] ProductoContext DbContext, [FromBody] Producto producto, [FromRoute] int id) =>
-{
-
-    var productoActual = DbContext.Productos.Find(id);
-
-    if (productoActual != null)
-    {
-        productoActual.Nombre = producto.Nombre;
-        productoActual.Detalle = producto.Detalle;
-        productoActual.Precio = producto.Precio;
-
-        await DbContext.SaveChangesAsync();
-
-        return Results.Ok();
-    }
-    return Results.NotFound();
-
-});
-
-app.MapDelete("/api/productos/{id}", async ([FromServices] ProductoContext dbContext, [FromRoute] int id) =>
-{
-    var productoActual = dbContext.Productos.Find(id);
-
-    if (productoActual != null)
-    {
-        dbContext.Remove(productoActual);
-        await dbContext.SaveChangesAsync();
-
-        return Results.Ok();
-    }
-
-    return Results.NotFound("Producto no encontrada");
-
-});
 
 app.Run();
